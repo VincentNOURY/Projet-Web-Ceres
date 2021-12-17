177425f1e747bfd72f0e0115c3337841668279b7
VincentNOURY <vincent.noury@efrei.net>
2021-12-17 11:45:09 +0100

    Added time and base for json data fetch

--- script/script.js
+++ script/script.js
@@ -10,3 +10,33 @@ function ajoute(){
   }
   table.appendChild(tr);
 }
+
+function getMeteo(){
+  fetch("http://www.infoclimat.fr/public-api/gfs/json\
+    ?_ll=48.85341,2.3488&_auth=AhhTRFMtBCYDLgA3D3lReFI6BjMPeQMkVysKaVo%2FUy4DaA\
+    BhAWFTNQdpUC0HKAo8V3pXNAE6AzNTOAN7CHpXNgJoUz9TOARjA2wAZQ8gUXpSfAZnDy8DJFc8C\
+    m9aKVM2A2UAegFjUzIHbVAsBzYKNldlVygBIQM6UzYDZwhhVzMCYFMzUzgEbwNkAH0PIFFjUmAG\
+    YA9mA2hXMQpsWjVTZQNmADcBYFM0B2BQLAc2CjxXYlcxATwDPlM2A2wIelcrAhhTRFMtBCYDLgA\
+    3D3lReFI0BjgPZA%3D%3D&_c=bc0b6c355d4a1f89b826625db56f406f")
+    .then((response) => response,json()).then(furction(data){
+    //define table with the data collected
+  })
+}
+
+getMeteo();
+
+function sleep(ms){
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function time(){
+  await sleep(1);
+  while (true){
+    date = new Date();
+    str = date.getHours() + " h " + date.getMinutes() + " m " + date.getSeconds() + " s";
+    document.querySelector("#time").innerHTML = str;
+    await sleep(1000);
+  }
+}
+
+time();
